5b14289c07035d92bddc1823fb9ecc049a77683a
Mario <recinos.mario68@gmail.com>
2021-11-03 19:35:48 -0700

    mongodb connection

--- server.js
+++ server.js
@@ -6,10 +6,22 @@ const mongoose = require('mongoose');
 const { application } = require('express');
 
 // Initialize Expres App
+const app = express();
 
 // Configure Settings
+require('dotenv').config();
+const {
+  DATABASE_URL,
+  PORT=3001
+} = process.env
 
 // Configure Connectionj to MongoDB
+mongoose.connect(DATABASE_URL);
+const db = mongoose.connection;
+
+db.on('connected', () => console.log('Connected to MongoDB'));
+db.on('disconnected', () => console.log('Disconnected to MongoDB'));
+db.on('error', (error) => console.log('MongoDB has an error ' + error.message));
 
 // Mount Middleware
 
