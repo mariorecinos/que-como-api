36f504ef3f4546233f5c3f3a7d943e1b074a1307
Mario <recinos.mario68@gmail.com>
2021-11-04 10:29:28 -0700

    added update route

--- controllers/stores.js
+++ controllers/stores.js
@@ -11,9 +11,28 @@ router.get('/', async (req, res) => {
   try {
       res.json(await Store.find({managedBy: req.user.uid}));
   } catch (error) {
-    res.status(401).json({message: 'Please login to see contacts'});
+    res.status(401).json({message: 'Please login to see stores'});
   }
 });
 
 // Delete Route
-router.delete()
+router.delete('/store/:id', async (req, res) => {
+    try {
+      res.json(await Store.findByIdAndDelete(req.params.id));
+    } catch (error) {
+      res.status(401).json({message: 'Sorry Something Went Wrong'})
+    }
+});
+
+// Update Route
+router.put('/store/:id', async (req, res) => {
+  try {
+      res.json(await Store.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true })
+      );
+  } catch (error) {
+    res.status(401).json({message: 'Sorry Something Went Wrong Updating Store'})
+  }
+})
