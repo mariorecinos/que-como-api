64e5ce743425d318f7f47a08940f4b633865674f
Mario <recinos.mario68@gmail.com>
2021-11-03 19:23:20 -0700

    Installing Dependencies

---
+++ server.js
@@ -0,0 +1,19 @@
+// Require Dependencies
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const { application } = require('express');
+
+// Initialize Expres App
+
+// Configure Settings
+
+// Configure Connectionj to MongoDB
+
+// Mount Middleware
+
+// Authorization Middleware
+
+// Tell The App To Listen
+app.listen(PORT, () => console.log(`Express is listening on port:${PORT}`));
